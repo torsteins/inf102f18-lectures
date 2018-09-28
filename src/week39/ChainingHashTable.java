010c27eceaa213b55d09400e9d94b65c6deae968
Torstein <torstein.stromme@ii.uib.no>
2018-09-28 14:02:50 +0200

    After lecture 39/2

--- src/week39/ChainingHashTable.java
+++ src/week39/ChainingHashTable.java
@@ -3,7 +3,7 @@ package week39;
 import week37.ISymTable;
 
 import java.lang.reflect.Array;
-import java.util.Iterator;
+import java.util.ArrayList;
 
 
 /**
@@ -11,29 +11,108 @@ import java.util.Iterator;
  */
 public class ChainingHashTable<Key extends Comparable<Key>, Value> implements ISymTable<Key, Value> {
 
+    private class Node {
+        Node next;
+        Key key;
+        Value value;
+
+        Node(Key key, Value value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    Node[] hashTable = (Node []) Array.newInstance(Node.class, 4);
+    int size = 0;
+
     @Override
     public void put(Key key, Value value) {
+        int hash = hashMod(key);
+        this.hashTable[hash] = insertKeyAtNode(key, value, this.hashTable[hash]);
+        if (this.size > hashTable.length*8) {
+            resize(hashTable.length * 2);
+        }
+    }
 
+    private Node insertKeyAtNode(Key key, Value value, Node node) {
+        if (node == null) {
+            this.size++;
+            return new Node(key, value);
+        }
+
+        if (node.key.equals(key)) {
+            node.value = value;
+            return node;
+        }
+
+        node.next = insertKeyAtNode(key, value, node.next);
+        return node;
+    }
+
+    private int hashMod(Key key) {
+        return (key.hashCode() & 0x7fffffff) % this.hashTable.length;
     }
 
     @Override
     public Value get(Key key) {
-        return null;
+        int hash = hashMod(key);
+        return getFromNode(key, this.hashTable[hash]);
+    }
+
+    private Value getFromNode(Key key, Node node) {
+        if (node == null) return null;
+
+        if (node.key.equals(key))
+            return node.value;
+
+        return getFromNode(key, node.next);
     }
 
     @Override
     public boolean containsKey(Key key) {
-        return false;
+        return get(key) != null;
     }
 
     @Override
     public void delete(Key key) {
+        int hash = hashMod(key);
+        this.hashTable[hash] = deleteFromNode(key, this.hashTable[hash]);
+        if (this.size < this.hashTable.length*2 && this.hashTable.length > 0) {
+            resize(this.hashTable.length/2);
+        }
+    }
+
+    private void resize(int newSize) {
+        Node[] oldHashTable = this.hashTable;
+        this.hashTable = (Node[]) Array.newInstance(Node.class, newSize);
+        this.size = 0;
+
+        for (int i = 0; i < oldHashTable.length; i++) {
+            while (oldHashTable[i] != null) {
+                Node node = oldHashTable[i];
+                put(node.key, node.value);
+                oldHashTable[i] = node.next;
+            }
+        }
 
     }
 
+    private Node deleteFromNode(Key key, Node node) {
+        if (node == null) return null;
+
+        if (node.key.equals(key)) {
+            this.size--;
+            return node.next;
+        }
+
+        node.next = deleteFromNode(key, node.next);
+        return node;
+    }
+
+
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
--- src/week39/SymTableTest.java
+++ src/week39/SymTableTest.java
@@ -1,9 +1,70 @@
 package week39;
 
 
+import week37.ISymTable;
+import week38.BinarySearchTree;
+import week38.RedBlackBST;
+
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import static helpers.Verify.*;
+import static helpers.Verify.verifyEquals;
+
 /**
  * @author Torstein Strømme
  */
 public class SymTableTest {
 
+
+    public static void main(String[] args) {
+        test(new BinarySearchTree<>());
+        test(new RedBlackBST<>());
+        test(new ChainingHashTable<>());
+    }
+
+    private static void test(ISymTable<String, Integer> bst) {
+        System.out.print("Sanity test of binary search tree: " + bst.getClass().getCanonicalName() + "... ");
+
+        verifyEquals(bst.size(), 0);
+        verifyFalse(bst.containsKey("A"));
+
+        bst.put("B", 4);
+        verifyEquals(bst.size(), 1);
+        bst.put("C", 5);
+        verifyEquals(bst.size(), 2);
+        bst.put("A", 3);
+        verifyEquals(bst.size(), 3);
+
+        verify(bst.containsKey("A"));
+        verify(bst.containsKey("B"));
+        verify(bst.containsKey("C"));
+
+        verifyFalse(bst.containsKey("D"));
+
+        verifyEquals(bst.get("B"), 4);
+
+
+        bst.delete("A");
+        verifyEquals(bst.size(), 2);
+
+        verifyFalse(bst.containsKey("A"));
+        verifyNull(bst.get("A"));
+
+        verify(bst.containsKey("B"));
+        verify(bst.containsKey("C"));
+
+        verifyEquals(bst.get("C"), 5);
+
+        bst.delete("D");
+        verifyEquals(bst.size(), 2);
+        verifyFalse(bst.containsKey("D"));
+
+        bst.put("A", 32);
+        verifyEquals(bst.get("A"), 32);
+        verifyEquals(bst.size(), 3);
+
+
+        System.out.println("all ok!");
+    }
 }
